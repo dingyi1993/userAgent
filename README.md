824f78aed0cea80ba49df0de10cfa6e6154b93f5
dingyi1993 <781745492@qq.com>
2016-12-31 16:17:16 +0800

    First version.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # userAgent
-Get an object which contains your browser's info using userAgent. 
+
+轻量级的检测ua，只判断了常用的设备和应对绝大部分的开发需求。
+
+支持amd、cmd，如果都你的项目中都不支持，则会导出到全局变量UA。
+
+##用法
+
+1. UA.isXXX
+
+    返回布尔值，获得当前环境是否是XXX，可以是浏览器、操作系统、app等，比如safari、Android、wechat（首字母都需大写）等。
+
+    具体支持哪些检测可以查看useragent.js或者查看UA对象的值。
+
+2. UA.is(value)
+
+    参数value为string，返回布尔值，获得当前ua是否包含特定的字符串，通常用于是否在app内的检测。
+
+3. UA.detail
+
+    返回一个对象，包含当前环境的详细信息，比如：
+
+        * 如果是IOS，则 UA.detail.os.version 会返回IOS的版本号
+
+        * 如果是chrome浏览器，则 UA.detail.browser.version 会返回chrome的版本号
+
+    这个对象支持的非常少，只是根据开发需求写了一部分。
--- useragent.js
+++ useragent.js
@@ -10,10 +10,8 @@
 }
 (function() {
     var ua = window.navigator.userAgent.toLowerCase();
-    console.log(ua);
-    alert(ua);
     var result = {
-        //os
+        // os
         isIOS: false,
         isAndroid: false,
         isWindows: false,
@@ -21,76 +19,121 @@
         isUnix: false,
         isMac: false,
 
-        //browser
+        // browser
         isSafari: false,
         isChrome: false,
         isIE: false,
+        isEdge: false,
         isFirefox: false,
         isOpera: false,
+        isUC: false,
 
-        //app
+        // app
         isWechat: false,
+        isWeibo: false,
+        isQQ: false,
 
-        //device
+        // device
         isIPhone: false,
         isIPad: false,
         isIPod: false,
         // isXiaomi: false,
 
+        isMobile: false,
+        isPC: false,
+
         detail: {
-            browser: {
-            },
-            engine: {
-            },
-            os: {
-            },
-            device: {
-            }
+            browser: {},
+            os: {},
+            app: {},
+            device: {}
         }
     };
 
     // os + device
-    if (/iphone/i.test(ua)) {
-        result.isIPhone = true;
-    } else if (/ipad/i.test(ua)) {
+    if (/ipad/i.test(ua)) {
+        result.detail.device.name = 'iPad';
         result.isIPad = true;
+    } else if (/iphone/i.test(ua)) {
+        result.detail.device.name = 'iPhone';
+        result.isIPhone = true;
     } else if (/ipod/i.test(ua)) {
+        result.detail.device.name = 'iPod';
         result.isIPod = true;
     } else if (/mac os x/i.test(ua)) {
+        result.detail.device.name = 'Mac';
         result.isMac = true;
     } else if (/android/i.test(ua)) {
+        var matchs = ua.match(/android[_ ](\d+)[_\.](\d+)([_\.]\d+)?[; ]/g);
+        result.detail.os.name = 'Android';
+        result.detail.os.version = matchs ? matchs[0].replace(/;/g, ' ').trim().replace(/_/g, ' ').split(' ')[1] : '0';
         result.isAndroid = true;
     } else if (/windows/i.test(ua)) {
+        result.detail.os.name = 'Windows';
         result.isWindows = true;
     } else if (/linux/i.test(ua)) {
+        result.detail.os.name = 'Linux';
         result.isLinux = true;
     } else if (/unix/i.test(ua)) {
+        result.detail.os.name = 'Unix';
         result.isUnix = true;
     }
 
     if (result.isIPhone || result.isIPad || result.isIPod) {
+        var matchs = ua.match(/os (\d+)[_\.](\d+)([_\.]\d+)? /g);
+        result.detail.os.version = matchs ? matchs[0].trim().split(' ')[1].replace(/_/g, '.') : '0';
         result.isIOS = true;
     }
 
     // browser
-    if (result.isIOS && /version\/(\d+)/i.test(ua) && !/chrome/i.test(ua)) {
-        result.isSafari = true;
+    if (/edge/i.test(ua)) {
+        result.detail.browser.name = 'Edge';
+        result.isEdge = true;
     } else if (/chrome|crios/i.test(ua)) {
+        var matchs = ua.match(/(chrome|crios)\/[\d\.]+ /g);
+        result.detail.browser.version = matchs ? matchs[0].trim().split('\/')[1] : '0';
+        result.detail.browser.name = 'Chrome';
         result.isChrome = true;
+    } else if (result.isIOS && /version\/(\d+)/i.test(ua)) {
+        var matchs = ua.match(/version\/[\d\.]+ /g);
+        result.detail.browser.version = matchs ? matchs[0].trim().split('\/')[1] : '0';
+        result.detail.browser.name = 'safari';
+        result.isSafari = true;
     } else if (/msie|trident/i.test(ua)) {
+        result.detail.browser.name = 'IE';
         result.isIE = true;
+    } else if (/ucbrowser/i.test(ua)) {
+        result.detail.browser.name = 'UC Browser';
+        result.isUC = true;
     } else if (/opera/i.test(ua)) {
+        result.detail.browser.name = 'Opera';
         result.isOpera = true;
     } else if (/firefox/i.test(ua)) {
+        result.detail.browser.name = 'Firefox';
         result.isFirefox = true;
     }
 
     // app
     if (/micromessenger/i.test(ua)) {
+        result.detail.app.name = 'Wechat';
         result.isWechat = true;
+    } else if (/weibo/i.test(ua)) {
+        result.detail.app.name = 'Weibo';
+        result.isWeibo = true;
+    } else if (/qq\//i.test(ua)) {
+        result.detail.app.name = 'QQ';
+        result.isQQ = true;
     }
-    alert(result.isChrome);
+
+    if (/mobile/i.test(ua)) {
+        result.isMobile = true;
+    } else {
+        result.isPC = true;
+    }
+
+    result.is = function(keyword) {
+        return !!keyword && ua.indexOf(keyword) > -1;
+    };
 
     return result;
 }));
-console.log(UA);
